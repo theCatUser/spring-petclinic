5ae1a89165856b6b58e87b2dfb6f334c665a175e
Catalin Dabija <Catalin.Dabija@endava.com>
2026-05-26 13:52:19 +0300

    Restructured the repo according to Monorepo

--- Terraform/flux.tf
+++
@@ -1,7 +0,0 @@
-# resource "flux_bootstrap_git" "this" {
-#   path = "clusters/aks-demo"
-
-#   depends_on = [
-#     azurerm_kubernetes_cluster.main
-#   ]
-# }
--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -2,14 +2,9 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
-    # azurerm = {
-    #   source  = "hashicorp/azurerm"
-    #   version = "~> 3.100"
-    # }
-
-    flux = {
-      source  = "fluxcd/flux"
-      version = "~> 1.7"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
     }
   }
 }
@@ -18,20 +13,3 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
 }
-
-# provider "flux" {
-#   kubernetes = {
-#     config_path = "~/.kube/config"
-#   }
-
-#   git = {
-#     url = var.flux_git_repository_url
-
-#     branch = var.flux_git_branch
-
-#     http = {
-#       username = var.flux_git_username
-#       password = var.flux_git_token
-#     }
-#   }
-# }
